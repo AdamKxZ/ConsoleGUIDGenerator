68197f565b748695ea04c2361b9ef1d0110986b9
AdamKxZ <9563953+AdamKxZ@users.noreply.github.com>
2020-08-30 12:41:06 -0700

    Add project files.

---
+++ GUIDGenerator/Generator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GUIDGenerator
+{
+    class Generator
+    {
+        public void GenerateGuid()
+        {
+            Console.WriteLine(Guid.NewGuid());
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public void StartProgram()
+        {
+            Console.WriteLine("Basic Console GUID Generator");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ GUIDGenerator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/* 
+ *    BASIC GUID GENERATOR
+ *    Written by AdamKxZ
+ */
+
+namespace GUIDGenerator
+{
+    class Program
+    {
+        string programName = "BASIC GUID GENERATOR";
+        string programVerson = "0.1";
+
+        static void Main(string[] args)
+        {
+            Generator gen = new Generator();
+            bool isValid = true;
+
+            gen.StartProgram();
+
+            Console.WriteLine("Press any key to generate a GUID.");
+            Console.ReadLine();
+
+            do
+            {
+                gen.GenerateGuid();
+                AskToContinue();
+            } while (isValid);
+
+            void AskToContinue()
+            {
+                bool askToContinue = true;
+                do
+                {
+                    Console.Write("Would you like to generate another? (Y)es or (N)o: ");
+                    Console.WriteLine();
+                    var answer = Console.ReadLine();
+
+                    if (answer == "Y" || answer == "y")
+                    {
+                        isValid = true;
+                        askToContinue = false;
+                    }
+                    else if (answer == "N" || answer == "n")
+                    {
+                        isValid = false;
+                        askToContinue = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR: Invalid Input. Must enter either 'Y' or 'N' only!");
+                        Console.WriteLine();
+                    }
+                } while (askToContinue);
+            }
+        }
+    }
+}
