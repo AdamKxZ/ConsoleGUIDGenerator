ad394761d059f2d49870b278c016f3dc8fb07c07
AdamKxZ <9563953+AdamKxZ@users.noreply.github.com>
2020-09-19 21:33:00 -0700

    Commit signing test 2

--- GUIDGenerator/Generator.cs
+++ GUIDGenerator/Generator.cs
@@ -35,7 +35,7 @@ namespace GUIDGenerator
         public void StartProgram()
         {
             // Useless starting function to clean up main function
-            Console.WriteLine("GUID Generator");
+            Console.WriteLine("GUID Generator v1.0");
             Console.WriteLine();
         }
 
